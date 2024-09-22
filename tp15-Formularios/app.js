f166479bb93bfd5a8e16c8e9e66b948131c6797b
aideity <luchesimartina@gmail.com>
2024-09-22 17:00:17 -0300

    Add files via upload

---
+++ tp15-Formularios/app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({ extended: true })); 
+
+app.post('/register', (req, res) => {
+    const { name, email, fecha, adress, category, avatar, password, Vpassword } = req.body;
+    
+    res.send('Registro exitoso');
+});
+
+
+app.post('/', (req, res) => {
+    const { password, Vpassword } = req.body;
+
+    if (password === Vpassword) {
+        res.redirect('/');
+    } else {
+        res.send('Las contraseñas no coinciden. <a href="/register">Volver al registro</a>');
+    }
+});
+
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+app.get('/login', (req,res) => res.sendFile(path.join(__dirname,'views','login.html')))
+app.get('/register', (req,res) => res.sendFile(path.join(__dirname,'views','register.html')))
+
+app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
